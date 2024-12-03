f0f8f37aae05e7c24e48d112b06d74914e654088
Wei Huo <77874700+hw-star@users.noreply.github.com>
2024-12-03 21:46:56 +0800

    Add files via upload

---
+++ codes/models/__init__.py
@@ -0,0 +1,12 @@
+import logging
+logger = logging.getLogger('base')
+
+def create_model(opt):
+    model = opt['model']
+    if model == 'STGAN_model':
+        from .STGAN_model import STGANModel as M
+    else:
+        raise NotImplementedError('Model [{:s}] not recognized.'.format(model))
+    m = M(opt)
+    logger.info('Model [{:s}] is created.'.format(m.__class__.__name__))
+    return m
